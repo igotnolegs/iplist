a209c2b9c4ee4a166aaed4b9022b9740f85cf259
U-EXCHANGE07\kevinf <kevinf@kevinf.exchange07.socen.com>
2009-06-17 10:26:45 -0600

    Whitespace fixed

--- managelists.php
+++ managelists.php
@@ -1,14 +1,16 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<?
-			include("style.css");
-			include("passwordz.php");
-			mysql_connect(localhost,$username,$password);
-			@mysql_select_db($database) or die("unable to connect to sql database");
-			$query = "SELECT * FROM IPListIndex ORDER BY IndexListTitle"; 
-			$result = mysql_query($query) or die(mysql_error());
-		?>
-		<title>Manage SCC IP Lists</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<?
+		include("style.css");
+		include("passwordz.php");
+		mysql_connect(localhost,$username,$password);
+		@mysql_select_db($database) or die("unable to connect to sql database");
+		$query = "SELECT * FROM IPListIndex ORDER BY IndexListTitle"; 
+		$result = mysql_query($query) or die(mysql_error());
+	?>
+	<title>Manage SCC IP Lists</title>
 	</head>
 	<body>
 		<table border="0" cellpadding="5" cellspacing="2">
@@ -20,23 +22,25 @@
 				<td>
 					<form action = "createnewlist.php" method = "post">
 						<p>Default IP Octet 1:<input type=	"text" name="defipoct1">	(3 digit integer)<br>
-						Default IP Octet 2:<input type=		"text" name="defipoct2">	(3 digit integer)<br>
-						Default IP Octet 3:<input type=		"text" name="defipoct3">	(3 digit integer)<br>
+						Default IP Octet 2:<input type=		  "text" name="defipoct2">	(3 digit integer)<br>
+						Default IP Octet 3:<input type=	  	"text" name="defipoct3">	(3 digit integer)<br>
 						Default Subnet Mask IP:<input type=	"text" name="defsubnetip">	(18 character string)<br>
-						Table Header/Title:<input type=		"text" name="deflisttitle">	(256 character string)<br>
-						Table name:<input type=				"text" name="deflistname">	(16 character string)
-						<input type=						"submit" value="Submit">
+						Table Header/Title:<input type=	  	"text" name="deflisttitle">	(256 character string)<br>
+						Table name:<input type=			       	"text" name="deflistname">	(16 character string)
+						<input type=			            			"submit" value="Submit">
 					</form>
 				</td>
 				<td>
+<!-- -->
 					<?
-						echo '<form name="deletelist" action="deletelist.php" method="post">
-						<select name="deletelist">';
+						echo "\t\t\t\t<form name=\"deletelist\" action=\"deletelist.php\" method=\"post\">\n",
+						"\t\t\t\t\t<select name=\"deletelist\">\n";
 						while($row = mysql_fetch_array($result)) {
-							echo '<option value="' . $row['IndexListID'] . '">' . $row['IndexListTitle'] . '</option>';
+							echo "\t\t\t\t\t\t<option value=\"", $row['IndexListID'], "\">", $row['IndexListTitle'], "</option>\n";
 						}
 						mysql_close();
 					?>
+<!-- -->
 					</select>
 						<input type="submit" value="Delete List"></form>
 				</td>
--- runfirst.php
+++ runfirst.php
@@ -1,5 +1,7 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
+	<meta http-equiv="Refresh" content="2; url=index.php; text/html; charset=iso-8859-1">
 		<title>IP LIST RUNFIRST</title>
 	</head>
 	<body>
@@ -9,24 +11,24 @@
 			@mysql_select_db($database) or die("unable to connect to sql database");
 		
 			$query1="CREATE TABLE IPListItem(
-				IPNumID			int(6) 			NOT NULL auto_increment PRIMARY KEY,
-				IPListOct1 		int(3) 			NOT NULL,
-				IPListOct2		int(3) 			NOT NULL,
-				IPListOct3		int(3) 			NOT NULL,
-				IPListOct4		int(3) 			NOT NULL,
-				IPListSNMask 	varchar(18) 	NOT NULL,
+				IPNumID 			  int(6) 			  NOT NULL auto_increment PRIMARY KEY,
+				IPListOct1 		  int(3) 			  NOT NULL,
+				IPListOct2		  int(3) 			  NOT NULL,
+				IPListOct3	  	int(3) 			  NOT NULL,
+				IPListOct4	  	int(3) 			  NOT NULL,
+				IPListSNMask  	varchar(18) 	NOT NULL,
 				IPListCustname	varchar(256) 	NOT NULL,
-				IPListID		varchar(16)		NOT NULL,
-				IPListTitle		varchar(256)	NOT NULL,
-				IPListTower		varchar(64)		NOT NULL,
-				IPListYDI		varchar(64)		NOT NULL)";
+				IPListID		    varchar(16)		NOT NULL,
+				IPListTitle	  	varchar(256)	NOT NULL,
+				IPListTower		  varchar(64)		NOT NULL,
+				IPListYDI	    	varchar(64)		NOT NULL)";
 			$query2="CREATE TABLE IPListIndex(
-				IndexNumID		int(6) 			NOT NULL auto_increment PRIMARY KEY,
-				IndexListID		varchar(16)		NOT NULL,
+				IndexNumID		  int(6)  			NOT NULL auto_increment PRIMARY KEY,
+				IndexListID		  varchar(16)		NOT NULL,
 				IndexListTitle	varchar(256)	NOT NULL)";
 			mysql_query($query1);
 			mysql_query($query2);
-			echo "DONE!";
+			echo "DONE!\n";
 		?>
 	</body>
 </html>
